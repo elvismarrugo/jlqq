31c7c97403e86db31f1a77983606839922d910ea
Elvix <elvismarrugo@gmail.com>
2024-09-18 17:25:53 -0500

    first commit

---
+++ src/config/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Montserrat_Alternates } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'], display: 'swap' }) 
+
+export const titleFont = Montserrat_Alternates({ subsets: ['latin'], weight: ['500', '700'], display: 'swap' })
