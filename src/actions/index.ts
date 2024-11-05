582258ad62686bbc961c930ec9ca1f80f5a33f63
Elvix <elvismarrugo@gmail.com>
2024-11-05 16:29:47 -0500

    -add login

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,9 @@
 
 
+export * from './auth/login';
+export * from './auth/logout';
+export * from './auth/register';
 
-export { getProductBySlug } from './product/get-product-by-slug';
-export { getStockBySlug } from './product/get-stock-by-slug';
-export { getPaginatedProductsWithImages } from './product/product-pagination';
+export * from './product/get-product-by-slug';
+export * from './product/get-stock-by-slug';
+export * from './product/product-pagination';
