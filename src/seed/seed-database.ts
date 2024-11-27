6e8a364744783991d94a383defd48dc62564becf
Elvix <elvismarrugo@gmail.com>
2024-11-27 10:57:51 -0500

    fix orders

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -7,9 +7,9 @@ async function main() {
 
   // 1. Borrar registros previos
   // await Promise.all( [
-  await prisma.OrderAddress.deleteMany();
-  await prisma.OrderItem.deleteMany();
-  await prisma.Order.deleteMany();
+  await prisma.orderAddress.deleteMany();
+  await prisma.orderItem.deleteMany();
+  await prisma.order.deleteMany();
 
   await prisma.userAddress.deleteMany();
   await prisma.user.deleteMany();
