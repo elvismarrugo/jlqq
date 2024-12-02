3c4d9aa09fbe4d22c8b76b77ec3a8f2a10daae84
Elvix <elvismarrugo@gmail.com>
2024-12-02 16:27:39 -0500

    fix sidebar mobile first

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -43,7 +43,8 @@ export const Sidebar = () => {
       {/* Sidemenu */}
       <nav
         className={clsx(
-          "fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300",
+          // "fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300",
+          "fixed p-5 right-0 top-0 w-[90%] max-w-xs h-screen bg-white z-20 shadow-2xl transform transition-all duration-300 sm:w-[500px]",
           {
             "translate-x-full": !isSideMenuOpen,
           }
