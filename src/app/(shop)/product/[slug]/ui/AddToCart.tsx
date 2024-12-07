7b8752323eb3f9fd80f0a1cfc81a8838c7220438
elvismarrugo <elvismarrugo@gmail.com>
2024-12-07 15:29:02 -0500

    ya se agregó

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -4,19 +4,19 @@ import { useState } from "react";
 
 import { QuantitySelector, SizeSelector } from "@/components";
 import type { CartProduct, Product, Size } from "@/interfaces";
-import { useCartStore } from '@/store';
+import { useCartStore } from "@/store";
 
 interface Props {
   product: Product;
 }
 
 export const AddToCart = ({ product }: Props) => {
-
-  const addProductToCart = useCartStore( state => state.addProductTocart );
+  const addProductToCart = useCartStore((state) => state.addProductTocart);
 
   const [size, setSize] = useState<Size | undefined>();
   const [quantity, setQuantity] = useState<number>(1);
   const [posted, setPosted] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const addToCart = () => {
     setPosted(true);
@@ -30,17 +30,20 @@ export const AddToCart = ({ product }: Props) => {
       price: product.price,
       quantity: quantity,
       size: size,
-      image: product.images[0]
-    }
+      image: product.images[0],
+    };
 
     addProductToCart(cartProduct);
     setPosted(false);
+    setSuccess(true); // Activar mensaje de éxito
     setQuantity(1);
     setSize(undefined);
 
-
+    // Desactivar mensaje de éxito después de 3 segundos
+    setTimeout(() => {
+      setSuccess(false);
+    }, 3000);
   };
-
 
   return (
     <>
@@ -48,6 +51,10 @@ export const AddToCart = ({ product }: Props) => {
         <span className="mt-2 text-red-500 fade-in">
           Debe de seleccionar una talla*
         </span>
+      )}
+
+      {success && (
+        <span className="mt-2 text-green-500 fade-in">Ya se agregó</span>
       )}
 
       {/* Selector de Tallas */}
